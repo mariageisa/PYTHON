4b1c9856b47ac50c6c7b957b3d2fc4c8486dad7d
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-10-18 19:53:37 -0300

    desafio 04 mostrando a m´dia de 4 notas

---
+++ aula10.18/desafio04.py
@@ -0,0 +1,8 @@
+#faça um programa que peça as 4 notas e mostre a média.
+
+nota1= input('informe a primeira nota:')
+nota2= input('informe a segunda nota:')
+nota3= input('informe a terceira nota')
+nota4= input('informe a quarta nota')
+
+print((int(nota1) + int(nota2) + int(nota3) + int(nota4))/4)
