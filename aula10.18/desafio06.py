cb1a4cde7a0a2dad61aed8faf310d8dbfb8a64f0
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-10-18 20:50:21 -0300

    desafio06 calculando a area de um quadrado e mostrando o dobro para o usuario

---
+++ aula10.18/desafio06.py
@@ -0,0 +1,6 @@
+#Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
+
+lado = input('informe o valor do lado de um quadrado:')
+
+print('o dobro desta área é: ', int(lado)*int(lado)*2)
+
