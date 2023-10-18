636a54d5c4272dc3ac034cb6bf5783358ef51019
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-10-18 16:09:44 -0300

    desafio 2

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,15 @@
+#1 Criar o arquivo desafio02.py na pasta da aula de hoje (18/10)
+#2 pedir para o usuario informar sua idade 
+#3 pedir para o usuario informar a idade de algum parente (pai,mãe,irmão)
+#4 pedir para o usuário informar o seu nome
+#5 imprimir a multiplicação das duas idades
+#6 imprimir o nome do usuário
+# OBS: somente idade dos parentes
+# FAÇA todos os commit e updates do GIT/GITHUB
+
+idade1 = input ('Digite sua idade aqui: ')
+idade2 = input ('Digite a idade de algum parente aqui(pai, mãe, irmão)')
+nome = input ('Digite seu nome aqui:')
+
+print(int(idade1)*int(idade2))
+print(nome)
