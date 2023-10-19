1843d411fcbe816da3a918413c6cd7176e60355f
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-10-19 14:42:55 -0300

    string alterada

--- aula10.18/desafio06.py
+++ aula10.18/desafio06.py
@@ -1,6 +1,6 @@
 #Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
 
-lado = input('informe o valor do lado de um quadrado:')
+lado = input('digite a altura ou a base de um quadrado:')
 
 print('o dobro desta área é: ', int(lado)*int(lado)*2)
 
