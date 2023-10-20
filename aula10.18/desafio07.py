7253f8837b49b759e9612df3a60dde738eb1f779
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-10-20 14:53:45 -0300

    salvando

--- aula10.18/desafio07.py
+++ aula10.18/desafio07.py
@@ -1,11 +1,11 @@
 #Faça um programa que pede 3 números inteiros. Calcule imprima: - o produto do dobro do primeiro com metade do segundo somado com o terceiro. A soma do triplo do primeiro com o terceiro e multiplicado pelo segundo.
 
-num1= input('digite um numero inteiro')
-num2= input('digite um numero inteiro')
-num3= input('digite um numero inteiro')
+a = input('digite um numero inteiro')
+b = input('digite um numero inteiro')
+c = input('digite um numero inteiro')
 
-calculo1= ((int(num1)*2)+ (int(num2)/2)+ int(num3))
-calculo2= ((int(num1)*3+ int(num3))*int(num2))
+calculo1= ((int(a)*2)* (int(b)/2)+ int(c))
+calculo2= ((int(a)*3+ int(c))*int(b))
 
 print (int(calculo1))
 print (int(calculo2))
