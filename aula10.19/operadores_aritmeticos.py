32b73a97d650ab34c5e678079662402b047e2bbf
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-10-21 21:12:29 -0300

    operadores aritimeticos

---
+++ aula10.19/operadores_aritmeticos.py
@@ -0,0 +1,12 @@
+# vamos fazer uma passagem sobre nossos operadores aritmeticos
+
+a = 3 + 3
+b = 5 - 3
+c = 4 * 3 
+d = 8 / 2 # divide mas só terá o valor dividendo
+e = 8 % 2 # resto da divisão
+f = 3**2 # a metada da metade
+
+print (float(e))
+
+
