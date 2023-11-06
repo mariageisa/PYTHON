3bf846fbaaf339c5ed9c3c53313d92842cc17883
mariageisa <104577350+mariageisa@users.noreply.github.com>
2023-11-06 16:33:49 -0300

    aula 06/11

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,14 @@
+# VARIAVEIS  são usadas para salvar algo na memoria do computador.
+# o sinal de = é usado para atribuicao de valores
+var01 = "alguma coisa"
+print(var01)
+var01 = "outra coisa"
+print(var01)
+var01 = 10
+print(var01)
+var02 = "A" + "B" + "C"
+print(var02)
+var02 = "A" * 10
+print(var02)
+var02 = 10 * " Paulo"
+print(var02)
